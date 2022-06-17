71597b1c98f4e7b3a8d049d15746d70ae7c355bf
Alexsoil <alexsoilgr@gmail.com>
2022-06-17 17:07:11 +0300

    Working for all data, plotting, small improvements still needed

--- src/question1/dbscan.py
+++ src/question1/dbscan.py
@@ -1,8 +1,9 @@
-from utils import read
+from utils import read, source_names
 import pandas as pd
 import numpy as np
 import os
 import seaborn as sns
+import matplotlib
 import matplotlib.pyplot as plt
 from datetime import datetime
 from kneed import KneeLocator
@@ -12,6 +13,9 @@ from sklearn.preprocessing import normalize
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
+THREADS = os.cpu_count()
+
+print("Go, go, go!")
 try:
     sources = pd.read_pickle('dataset' + os.path.sep + 'pickleJar' + os.path.sep + 'sources.pkl')
     demand = pd.read_pickle('dataset' + os.path.sep + 'pickleJar' + os.path.sep + 'demand.pkl')
@@ -22,42 +26,54 @@ except:
     sources.to_pickle('dataset' + os.path.sep + 'pickleJar' + os.path.sep + 'sources.pkl')
     demand.to_pickle('dataset' + os.path.sep + 'pickleJar' + os.path.sep + 'demand.pkl')
     print('Data pickled')
-print(sources.describe())
-print(sources.shape)
+
+# Proprocessing
 for name, values in sources.iteritems():
     sources[name].fillna(round(sources[name].median(skipna = True)), inplace = True)
-print(sources.describe())
+print("Preprocessing complete")
+
 sources = sources.resample('1d').mean()
 sources['Datetime'] = sources.index
-solar = sources[['Datetime', 'Solar']]
-for idx, row in solar.iterrows():
-    solar.loc[idx, 'Datetime'] = int(row['Datetime'].timetuple().tm_yday)
-print(solar)
-days = solar['Datetime'].to_numpy()
-production = solar['Solar'].to_numpy()
+print(sources)
 
-nn = NearestNeighbors(n_neighbors=4)
-neighbors = nn.fit(solar.to_numpy())
+# Plot a graph of production for each type of power source
+for source in source_names:
+    df = sources[['Datetime', source]].copy()
+    df['DayofYear'] = np.nan
+    # Convert datetime format to day of year (1-365)
+    for idx, row in df.iterrows():
+        df.loc[idx, 'DayofYear'] = int(row['Datetime'].timetuple().tm_yday)
+    data = df.to_numpy()
+    min_pts = 4 #Minimum items in a cluster (Subject to change)
+    print(min_pts)
+    # Find optimal epsilon value
+    nn = NearestNeighbors(n_neighbors=min_pts)
+    neighbors = nn.fit(data[:, 2:0:-1])
+    distances, indices = neighbors.kneighbors(data[:, 2:0:-1])
+    distances = np.sort(distances[:, min_pts-1], axis=0)
+    i = np.arange(len(distances))
+    knee = KneeLocator(i, distances, S=1, curve='convex')
+    epsilon = distances[knee.knee]
+    # Apply the DBSCAN algorithm on the dataset
+    dbscan_cluster = DBSCAN(eps=epsilon, min_samples=min_pts, n_jobs=THREADS)
+    dbscan_cluster.fit(data[:, 2:0:-1])
+    # Plot the Graph
+    plt.figure(num=source, figsize=(5, 5))
+    scatter = plt.scatter(data[:, 2], data[:, 1], c=dbscan_cluster.labels_, s=10, cmap='Set1')
+    labels = [-1]
+    for i in range(len(dbscan_cluster.labels_) - 1):
+        labels.append(i)
+    plt.legend(handles=scatter.legend_elements()[0], title= 'Cluster', labels=labels)
+    plt.xlabel("Days")
+    plt.ylabel(source)
+    # Print outliers
+    labeled_data = np.append(data, np.transpose([dbscan_cluster.labels_]), axis=1)
+    output = pd.DataFrame(data=labeled_data, columns=['Datetime', 'Value', 'DayofYear', 'Label'])
+    print(source + " outliers: (Total " + str(list(dbscan_cluster.labels_).count(-1)) + ")")
+    for idx, row in output.iterrows():
+        if row['Label'] == -1:
+            print(row['Datetime'].strftime('%Y-%m-%d'))
+    print(source + " Complete")
 
-distances, indices = neighbors.kneighbors(solar.to_numpy())
-distances = np.sort(distances[:, 3], axis=0)
-
-
-i = np.arange(len(distances))
-knee = KneeLocator(i, distances, S=1, curve='convex')
-
-fig = plt.figure(figsize=(5, 5))
-knee.plot_knee()
-plt.xlabel("Points")
-plt.ylabel("Distance")
-plt.show()
-print(distances[knee.knee])
-dbscan_cluster1 = DBSCAN(eps=distances[knee.knee], min_samples=4)
-dbscan_cluster1.fit(solar.to_numpy())
-
-plt.scatter(days, production, c=dbscan_cluster1.labels_)
-plt.xlabel("Days")
-plt.ylabel("Production")
-
-# sns.scatterplot(x='Datetime', y='value', data=solar.melt(['Datetime']))
+# Show all Graphs
 plt.show()
