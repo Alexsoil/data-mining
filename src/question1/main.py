d92a670437d8baae6b6ada35727435951033a48b
Vas-Laptop <PDFOrigami@gmail.com>
2022-06-24 17:49:19 +0300

    Add remaining 1.1 graphs

--- src/question1/main.py
+++ src/question1/main.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from dbscan import outlier_detector
 import utils
 
+sns.set_theme()
+
 temp = utils.load_data()
 sources = utils.fill_nan(temp[0])
 demand = utils.fill_nan(temp[1])
@@ -12,12 +14,56 @@ demand = utils.fill_nan(temp[1])
 # print(sources.describe())
 # print(demand)
 # print(demand.describe())
-sample = sources.resample("7d").mean().fillna(0)
-sample['Datetime'] = sample.index
-sns.lineplot(x='Datetime', y='value', hue="variable", data=sample.melt(['Datetime']))
-plt.show()
-outlier_detector(sources, utils.source_names)
-outlier_detector(demand, utils.demand_dtype)
+
+# outlier_detector(sources, utils.source_names)
+# outlier_detector(demand, utils.demand_dtype)
+
+
+def all_sources():
+    sample = sources.resample("1H").mean().fillna(0)
+    sample = sample.apply(lambda x: x.rolling("60D", min_periods=24 * 30).mean())
+
+    sample['Date'] = sample.index
+    df = (
+        sample.melt(['Date'])
+        .rename(columns={"variable": "Source", "value": "Megawatts"})
+    )
+    sns.lineplot(x='Date', y='Megawatts', hue="Source", data=df)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.show()
+
+
+def duck_curve():
+    start = pd.to_datetime('2021-08-15 00:00:00')
+    end = pd.to_datetime('2021-08-15 23:55:00')
+    day_sources = sources.loc[start:end]
+    day_demand = demand.loc[start:end]['Current demand']
+    day_dispatchable = day_sources.drop(['Solar', 'Wind'], axis=1)
+
+    df = pd.DataFrame()
+    df.index = day_sources.index
+    df['Demand'] = day_demand
+    df['Solar & wind'] = day_sources['Solar'] + day_sources['Wind']
+    df['Dispatchable'] = day_dispatchable.sum(axis=1)
+
+    df.index = df.index.hour
+
+    df = df.melt(ignore_index=False).reset_index()
+    df = df.rename(columns={
+        "variable": "Variable",
+        "value": "Megawatts",
+        "Datetime": "Hour"
+    })
+
+    sns.lineplot(x=df['Hour'], y='Megawatts', hue='Variable', data=df).set_title(
+        "Hourly demand vs. dispatchable sources vs. solar & wind (August 15, 2021)"
+    )
+
+    plt.xticks(range(0, 24, 2))
+    plt.show()
+
+
+all_sources()
 
 
 def supply_demand():
@@ -85,4 +131,4 @@ def source_mix():
     sns.barplot(data=mix, x="Percentage", y="Source").set_title("Energy production mix")
 
 
-sns.set_theme()
+
